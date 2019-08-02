debd3bae4407990a920402dcd7929722b3454874
joost aafjes <joostaafjes@gmail.com>
2019-08-02 17:33:04 +0200

    convert image to rgb before classifying

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,10 +1,13 @@
 import os
 import time
+import calendar
 
 import tensorflow as tf
 from keras.preprocessing.image import array_to_img, img_to_array, load_img
 from PIL import Image, ImageOps
 import numpy as np
+
+import cv2
 
 from object_classifier import ObjectClassifier
 from color_classifier import ColorClassifier
@@ -30,6 +33,8 @@ class TLClassifier(object):
         
         rospy.loginfo('TLClassifier inititialized')
 
+        self.RECORD_CROPPED_IMAGES = False
+
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
         Args:
@@ -38,6 +43,9 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
         """
         rospy.loginfo('TLClassifier received image')
+
+        # convert from bgr 2 rgb
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         # step 1
         traffic_light_images = self.object_classifier.get_traffic_light_images(image)
@@ -49,6 +57,15 @@ class TLClassifier(object):
         tf_color = ['RED', 'YELLOW', 'GREEN', 'UNDEFINED', 'UNKNOWN'] # Not so nice ;-( but want to show text
         rospy.loginfo('Traffic light detected {}'.format(tf_color[traffic_light_color]))
         #changed bacue now using styx msg #TrafficLight(traffic_light_color)))
+
+        if self.RECORD_CROPPED_IMAGES:
+            dir = './data/cropped/'
+            if not os.path.exists(dir):
+                os.makedirs(dir)
+
+            for idx, image in enumerate(traffic_light_images):
+                f_name = "sim_tl_{}_{}_{}.jpg".format(calendar.timegm(time.gmtime()), tf_color[traffic_light_color], idx)
+                image.save(dir + f_name)
 
         return traffic_light_color
 
